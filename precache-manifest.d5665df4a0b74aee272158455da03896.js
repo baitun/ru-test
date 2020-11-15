58fb1570f2af5e647819ceb21c2ef9feff82dd42
Yury Savin <yury@savin.dev>
2020-11-16 00:06:56 +0100

    Updates

---
+++ precache-manifest.d5665df4a0b74aee272158455da03896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b371e0406f4771af4b24c2cdd900a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db9cd0b46eab5409bf61",
+    "url": "/static/css/main.60e86531.chunk.css"
+  },
+  {
+    "revision": "e1655aaec5db69fda05d",
+    "url": "/static/js/2.b81fa5ca.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.b81fa5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9cd0b46eab5409bf61",
+    "url": "/static/js/main.1ebe0ec2.chunk.js"
+  },
+  {
+    "revision": "5af37f3f2ca351ecd263",
+    "url": "/static/js/runtime-main.9f3acdf8.js"
+  }
+]);
