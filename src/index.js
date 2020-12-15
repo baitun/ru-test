c0be9c82a2753b40b2f9e078cc353f94828b8b9c
Yury Savin <yury@savin.dev>
2020-12-15 02:59:35 +0100

    ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -6,14 +6,23 @@ import * as Sentry from "@sentry/react";
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn:
-      "https://59fb7f6408f14295be3845cf393face8@o293087.ingest.sentry.io/5517954"
+      "https://59fb7f6408f14295be3845cf393face8@o293087.ingest.sentry.io/5517954",
   });
+}
+
+function FallbackComponent({ error }) {
+  return (
+    <>
+      <h1>Что-то пошло не так...</h1>
+      <h2>{error.toString()}</h2>
+    </>
+  );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+    <Sentry.ErrorBoundary fallback={FallbackComponent}>
       <div className="container">
         <App />
       </div>
