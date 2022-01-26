c53710a1afeec47cadc280397a607fa3fae8bae0
Yury Savin <yury@savin.dev>
2022-01-26 23:11:39 +0100

    update sentry

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
-    dsn:
-      "https://59fb7f6408f14295be3845cf393face8@o293087.ingest.sentry.io/5517954",
+    dsn: "https://59fb7f6408f14295be3845cf393face8@o293087.ingest.sentry.io/5517954",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
   });
 }
 
